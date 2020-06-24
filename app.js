272e1f1db91ba82c6f9cd4b0a80e7a4d115a4b89
Linh Nguyen <54815653+halinh0507@users.noreply.github.com>
2020-06-24 13:27:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const GOOGLE_GEOCODING_API = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-const GOOGLE_GEOCODING_API_KEY = process.env.GOOGLE_GEOCODING_API_KEY;
+const GOOGLE_GEOCODING_API = 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCCgu68cvlHmJGbNvd7XcfZ6U_B8rfVj14';
+const GOOGLE_GEOCODING_API_KEY = 'AIzaSyCCgu68cvlHmJGbNvd7XcfZ6U_B8rfVj14';
 // Imports dependencies and set up http server
 const 
   request = require('request'),
